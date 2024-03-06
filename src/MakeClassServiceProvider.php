ea6f472473feea5233842e3916ca13fcede3b377
Sandro Cagara <sandrocagaragithub@gmail.com>
2024-03-06 16:53:46 +0800

    forgot service provider

---
+++ src/MakeClassServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Laracrea\LaravelClassGenerator;
+
+use Illuminate\Support\ServiceProvider;
+use Laracrea\LaravelClassGenerator\Console\MakeClassCommand;
+
+class MakeClassServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([MakeClassCommand::class]);
+        }
+    }
+
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
